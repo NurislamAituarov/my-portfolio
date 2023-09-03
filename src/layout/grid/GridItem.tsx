d0c8cfc97890d25af7c2b6cb892af61fbd4ec23d
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-09-03 22:27:01 +0600

    add popup options

--- src/layout/grid/GridItem.tsx
+++ src/layout/grid/GridItem.tsx
@@ -9,6 +9,8 @@ interface IGridItem {
 }
 
 export function GridItem({ item }: IGridItem) {
+  function handelMore() {}
+
   return (
     <a href={item.link} rel="noreferrer" target="_blank" className={styles.item}>
       <div
@@ -16,8 +18,19 @@ export function GridItem({ item }: IGridItem) {
         style={{
           backgroundImage: `linear-gradient(to right, ${item.gradient.from} 0%, ${item.gradient.to} 51%, ${item.gradient.from} 100%)`,
         }}>
-        {/* <Image src={item.icon.path} alt={item.title} width={item.icon.width} height={76} /> */}
         <Icon name={item.icon.name} />
+
+        <div
+          className={styles['popup-options']}
+          style={{
+            backgroundImage: 'linear-gradient(to right, #000000 0%, #b40808 51%, #980000 100%)',
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            handelMore();
+          }}>
+          <span>Подробнее</span>
+        </div>
       </div>
       <div className={styles.title}>{item.title}</div>
       {item._id === 'link_2' && <AnimateBlock />}
