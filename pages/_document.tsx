3681c8b17fd2d7a909f7fa46c963c751b8860b5c
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-09-15 22:06:13 +0600

    rever

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import favicon from '../public/favicon.ico';
-import CustomHead from '@/components/head/CustomHead';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <CustomHead />
       <body>
         <Main />
         <div id="portal" />
