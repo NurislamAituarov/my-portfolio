c2d3e12089c3ee840363fa7a2620d6312d22a56f
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-09-17 20:32:52 +0600

    add a check for the development process

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
+  const pathFavicon =
+    process.env.NODE_ENV === 'development' ? '/favicon.ico' : '/my-portfolio/favicon.ico';
+
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href={pathFavicon} sizes="any" />
       </Head>
       <body>
         <Main />
