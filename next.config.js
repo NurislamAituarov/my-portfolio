6c3dd8c9b0da17d688fe90de54863720ac665de2
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-11-26 17:00:32 +0600

    domain fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: ['localhost', 'i.ytimg.com', 'nurislamaituarov.github.io'] },
+  images: { domains: ['localhost', 'i.ytimg.com'] },
   env: {
     APP_URL: process.env.REACT_APP_URL,
     APP_ENV: process.env.REACT_APP_ENV,
